870f2032e873516a647f0185888dd049df811975
ewasniak <ethan.wasniak@gmail.com>
2026-04-03 23:04:13 -0400

    Corected some var names. Added some comments.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <SFML/Graphics.hpp>
-#include "physicsManager.h"
+#include "PhysicsManager.h"
 #include "GameManager.h"
+
+/**CLARIFICATIONS FOR DEVELOPERS:
+ * In SFML, the coordinate system for a window of dimensions {width, height} has the following corner coordinates:
+ * -Top-Left: (0,0)
+ * -Top-Right: (width, 0)
+ * -Bottom-Left: (0, height)
+ * -Bottom-Right: (width, height)
+ * 
+ * In order to compensate for the fact that the y-value increases as you go down in the window, a negative y-value
+ * in the velocity will mean that the ball is going up, and a positive mean that it is going down. 
+ */
 
 int main(){
     sf::RenderWindow window(sf::VideoMode({800, 850}), "Breakout Game");
-    window.setFramerateLimit(950); //950
+    window.setFramerateLimit(950); //To smooth the speed of the game/ball to be more consistient (not entierly working rn, looking into it)
 
     sf::RectangleShape paddle({150.f, 20.f});
     paddle.setOrigin({75.f, 10.f});
@@ -19,37 +30,32 @@ int main(){
 
     GameManager GameManager;
     
-    float xOffset = 10.f; //border of the game area offset for the x-axis (left & right)
-    float yOffset = 10.f; //border of the game area offset for the y-axis (top & bottom)
-    float paddleYPos = 790.f; //height/y-position of the paddle
-    float killY = 805.f; //y-position at which the ball will be considered as out of bounds and result in the loss of a life
-
-    int lives = 3;
+    const float X_OFFSET = 10.f; //border of the game area offset for the x-axis (left & right)
+    const float Y_OFFSET = 10.f; //border of the game area offset for the y-axis (top & bottom)
+    const float PADDLE_Y_POS = 790.f; //height/y-position of the paddle
 
     while (window.isOpen()){
         while(auto event = window.pollEvent()){
             if(event->is<sf::Event::Closed>()){
                 window.close();
             }
-
         }
         window.clear(sf::Color(0,0,0)); //clear frame before rewrite
 
         //borders
-        std::array leftLine = {sf::Vertex{sf::Vector2f(xOffset, yOffset)}, sf::Vertex{sf::Vector2f(xOffset, (window.getSize().y - yOffset))}};
+        std::array leftLine = {sf::Vertex{sf::Vector2f(X_OFFSET, Y_OFFSET)}, sf::Vertex{sf::Vector2f(X_OFFSET, (window.getSize().y - Y_OFFSET))}};
         window.draw(leftLine.data(), leftLine.size(), sf::PrimitiveType::Lines); //left wall
-        std::array topLine = {sf::Vertex{sf::Vector2f(xOffset, yOffset)}, sf::Vertex{sf::Vector2f((window.getSize().x - xOffset), yOffset)}};
+        std::array topLine = {sf::Vertex{sf::Vector2f(X_OFFSET, Y_OFFSET)}, sf::Vertex{sf::Vector2f((window.getSize().x - X_OFFSET), Y_OFFSET)}};
         window.draw(topLine.data(), topLine.size(), sf::PrimitiveType::Lines); //top wall 
-        std::array rightLine = {sf::Vertex{sf::Vector2f((window.getSize().x - xOffset), yOffset)}, sf::Vertex{sf::Vector2f((window.getSize().x - xOffset), (window.getSize().y - yOffset))}};
+        std::array rightLine = {sf::Vertex{sf::Vector2f((window.getSize().x - X_OFFSET), Y_OFFSET)}, sf::Vertex{sf::Vector2f((window.getSize().x - X_OFFSET), (window.getSize().y - Y_OFFSET))}};
         window.draw(rightLine.data(), rightLine.size(), sf::PrimitiveType::Lines); //right wall
 
-        
         //paddle 
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)){ //move left
-            paddle.setPosition({(paddle.getPosition().x - 0.1f > (xOffset + (paddle.getSize().x / 2)) ? paddle.getPosition().x - 0.1f : (xOffset + (paddle.getSize().x / 2))), paddleYPos});
+            paddle.setPosition({(paddle.getPosition().x - 0.1f > (X_OFFSET + (paddle.getSize().x / 2)) ? paddle.getPosition().x - 0.1f : (X_OFFSET + (paddle.getSize().x / 2))), PADDLE_Y_POS});
         }
         else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)){ //move right
-            paddle.setPosition({(paddle.getPosition().x + 0.1f < (window.getSize().x - (xOffset + (paddle.getSize().x / 2))) ? paddle.getPosition().x + 0.1f : (window.getSize().x - (xOffset + (paddle.getSize().x / 2)))), paddleYPos});
+            paddle.setPosition({(paddle.getPosition().x + 0.1f < (window.getSize().x - (X_OFFSET + (paddle.getSize().x / 2))) ? paddle.getPosition().x + 0.1f : (window.getSize().x - (X_OFFSET + (paddle.getSize().x / 2)))), PADDLE_Y_POS});
         }
 
         if(GameManager.haveDied()){ paddle.setFillColor(sf::Color::Red); } //life tracking and general game manager testing
@@ -57,15 +63,15 @@ int main(){
         
         //ball
         ball.setPosition(ball.getPosition() + PhysicsManager.getVelocity());
-        if(ball.getPosition().x >= (window.getSize().x - (xOffset + ball.getRadius())) || ball.getPosition().x <= (xOffset + ball.getRadius())){ PhysicsManager.reflectX(); } //bounce off left or right wall
-        if(ball.getPosition().y <= (yOffset + ball.getRadius())){ PhysicsManager.reflectY(); } //bounce off top wall/ceiling 
-        if(ball.getPosition().y >= (paddleYPos-(paddle.getSize().y/2)-(ball.getRadius())) && 
+        if(ball.getPosition().x >= (window.getSize().x - (X_OFFSET + ball.getRadius())) || ball.getPosition().x <= (X_OFFSET + ball.getRadius())){ PhysicsManager.reflectX(); } //bounce off left or right wall
+        if(ball.getPosition().y <= (Y_OFFSET + ball.getRadius())){ PhysicsManager.reflectY(); } //bounce off top wall/ceiling 
+        if(ball.getPosition().y >= (PADDLE_Y_POS-(paddle.getSize().y/2)-(ball.getRadius())) && 
            ((ball.getPosition().x > (paddle.getPosition().x - (paddle.getSize().x/2) - ball.getRadius()/2)) && 
             (ball.getPosition().x < (paddle.getPosition().x + (paddle.getSize().x/2) + ball.getRadius()/2))) &&
            (PhysicsManager.getVelocity().y > 0)){
             PhysicsManager.reflectY(); 
         }
-        if(ball.getPosition().y > (paddleYPos-(paddle.getSize().y/2)-(ball.getRadius())+10)){
+        if(ball.getPosition().y > (PADDLE_Y_POS-(paddle.getSize().y/2)-(ball.getRadius())+10)){
             GameManager.loseLife();
             ball.setPosition({400.f, 766.f});
             PhysicsManager.setVelocity({0.1f,-0.1f});
